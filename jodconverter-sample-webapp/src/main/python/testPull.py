9636927ad3562eb892d11a31503634b0ddc8433a
Alan Arvesen <alan.arvesen@handshakez.com>
2014-02-18 09:59:57 -0600

    coarse grained timing

--- jodconverter-sample-webapp/src/main/python/testPull.py
+++ jodconverter-sample-webapp/src/main/python/testPull.py
@@ -1,13 +1,18 @@
 import requests
+import time
 import settings
 files= { 'file' : open(settings.infile, 'rb')}
 
+start = time.time()
+print "Staring request..."
 r = requests.post(settings.url, files=files, stream=True)
+end = time.time()
+print "Elapsed: {}".format(end - start)
 if r.status_code != requests.codes.ok:
-    print "Error: %".format(r.status_code)
+    print "Error: {}".format(r.status_code)
 else:
     chunk_size = 2048
-    thumbnail = "/tmp/tmp.pdf"
+    thumbnail = "/tmp/tmp.png"
     with open(thumbnail, 'wb') as f:
         for chunk in r.iter_content(chunk_size):
             f.write(chunk)
