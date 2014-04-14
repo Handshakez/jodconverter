a7f910014179f2873aa72661536f0f69c8dd5a3e
Alan Arvesen <alan.arvesen@handshakez.com>
2014-04-14 11:18:38 -0500

    Add missing file

---
+++ pdfserver/src/main/java/com/mydocket/pdfmaker/converter/ThumbAndReturn.java
@@ -0,0 +1,19 @@
+package com.mydocket.pdfmaker.converter;
+
+import java.io.File;
+
+// When OO was importing a PDF, it would try to convert it into a giant
+// list of postscript pages.  As in: actual code.
+// So if you get a PDF, just run the thumbnail code and return
+// the original file.
+public class ThumbAndReturn {
+
+	private MakeAThumb makeAThumb = new MakeAThumb();
+	
+	public void execute(Observer observer, File input) throws Exception {
+		File thumb = makeAThumb.doThumb(input, input);
+		observer.observe(thumb);
+		observer.observe(input);
+	}
+	
+}
